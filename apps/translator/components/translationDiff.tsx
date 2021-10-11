434b01862ae3a26d9e3cfa84d83bd2f3f7d9c133
Jordan <jordan@jpdesigning.com>
2021-10-11 17:18:57 -0400

    fix(translator): hide translation diffs when diffs is an empty array

--- apps/translator/components/translationDiff.tsx
+++ apps/translator/components/translationDiff.tsx
@@ -1,5 +1,5 @@
 import { ReactElement } from "react";
-import { IDiffArrayItem, selectDiffApiItems, useAppSelector } from "../app";
+import { selectDiffApiItems, useAppSelector } from "../app";
 
 
 const TranslationDiff = (): ReactElement => {
@@ -7,11 +7,13 @@ const TranslationDiff = (): ReactElement => {
 
     return (
         <>
-            <h2 className="text-3xl leading-6 font-bold text-gray-900 mb-4 capitalize">Cypress Api Methods Used</h2>
-            <hr />
-            <ul className="list-inside list-disc p-4 mb-4">
-                {diff.map((d, i: number) => <li key={i}><a className="text-green-400" href={d.url} rel="noreferrer" target='_blank'>{d.command}</a></li>)}
-            </ul>
+            {diff.length > 0 ?  <>
+                <h2 className="text-3xl leading-6 font-bold text-gray-900 mb-4 capitalize">Cypress Api Methods Used</h2>
+                <hr />
+                <ul className="list-inside list-disc p-4 mb-4">
+                    {diff.map((d, i: number) => <li key={i}><a className="text-green-400" href={d.url} rel="noreferrer" target='_blank'>{d.command}</a></li>)}
+                </ul>
+            </> : null}
         </>
     )
 }
