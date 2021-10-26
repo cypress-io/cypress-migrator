3af96cf8fca04d38220f4a5a8595b1190036f936
Adam Murray <adamm@cypress.io>
2021-10-26 11:10:37 -0500

    update e2e test spec. update workflow steps. add ci test job

--- apps/translator-e2e/src/integration/translator.spec.js
+++ apps/translator-e2e/src/integration/translator.spec.js
@@ -5,37 +5,33 @@ describe('Translator app', () => {
     cy.visit('/')
   })
 
-  it('displays logo and nav links correctly', () => {
+  it('correctly displays and translates', () => {
+    // displays logo and nav links correctly
     cy.get('[alt="Cypress Logo"]').click()
     cy.location().should((loc) => {
       expect(loc.pathname).to.eq('/')
     })
     cy.getBySel('nav-list').find('li').should('have.length', 2)
-  })
 
-  it('can select language to be translated', () => {
+    // correctly selects language
     cy.getBySel('language-select-button').find('span').contains('protractor')
     cy.getBySel('language-select-button').click()
     cy.getBySel('language-select-options').should('have.length', 1)
-    cy.getBySel('language-select-options').select('protractor')
-
+    cy.getBySel('language-select-options').contains('li', 'protractor').click()
     // test page elements that are dynamic based on selected language
-    cy.getBySel('nav-list').find('li').first().contains('protractor')
-    cy.getBySel('all-translations-link').find('span').contains('protractor')
-  })
+    cy.getBySel('nav-list').contains('li', 'protractor')
+    cy.getBySel('all-translations-link').contains('span', 'protractor')
 
-  it('can correctly translate code and show more information', () => {
+    // correctly translates code
     cy.get('.original-in-monaco-diff-editor').find('.view-line').should('have.length', 7)
     cy.get('.modified-in-monaco-diff-editor').find('.view-line').should('have.length', 1)
     cy.getBySel('translate-button').click()
     cy.get('.modified-in-monaco-diff-editor').find('.view-line').should('have.length', 7)
 
-    // more details section
+    // shows more details section
     cy.getBySel('more-details').should('be.visible')
     cy.getBySel('more-details').find('h2').should('be.visible')
     cy.getBySel('api-details').find('p').should('be.visible')
     cy.getBySel('api-details-list').find('li').should('have.length', 4)
-    // cy.getBySel('api-details-list').find('li').first().should('contain', 'visit')
-    // cy.getBySel('api-details-list').find('').first().should('have.attr', 'href', 'https://on.cypress.io/visit')
   })
 })
