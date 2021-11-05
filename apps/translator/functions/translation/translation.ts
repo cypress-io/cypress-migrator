4617deb12187dbb924a8daa04b7f730b08761270
Jordan <jordan@jpdesigning.com>
2021-11-05 09:25:45 -0400

    fix(translator): add logging to translation function

--- apps/translator/functions/translation/translation.ts
+++ apps/translator/functions/translation/translation.ts
@@ -3,8 +3,12 @@ import { Handler, HandlerContext, HandlerEvent } from '@netlify/functions'
 export const handler: Handler = async (event: HandlerEvent, context: HandlerContext) => {
   const { protractor, cypress }: { protractor: string; cypress: string } = JSON.parse(event.body)
 
+  const body = JSON.stringify(protractor === cypress ? { protractor } : { protractor, cypress })
+
+  console.log(body)
+
   return {
     statusCode: 200,
-    body: JSON.stringify(protractor === cypress ? { protractor } : { protractor, cypress }),
+    body,
   }
 }
