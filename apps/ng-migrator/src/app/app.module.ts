5a56b3fe900b82cfee265bf67840e2d03d6b4906
Jordan <jordan@jpdesigning.com>
2021-12-16 19:05:30 -0500

    feat(ng-migrator): update select language component

--- apps/ng-migrator/src/app/app.module.ts
+++ apps/ng-migrator/src/app/app.module.ts
@@ -8,11 +8,18 @@ import { EditorModule } from './editor/editor.module'
 import { NavigationComponent } from './navigation/navigation.component'
 import { FooterComponent } from './footer/footer.component'
 import { LanguagePillsComponent } from './language-pills/language-pills.component'
-import { SelectListComponent } from './select-list/select-list.component';
 import { NotificationComponent } from './notification/notification.component'
+import { SelectLanguageComponent } from './select-language/select-language.component'
 
 @NgModule({
-  declarations: [AppComponent, NavigationComponent, FooterComponent, LanguagePillsComponent, SelectListComponent, NotificationComponent],
+  declarations: [
+    AppComponent,
+    NavigationComponent,
+    FooterComponent,
+    LanguagePillsComponent,
+    SelectLanguageComponent,
+    NotificationComponent,
+  ],
   imports: [
     BrowserModule,
     RouterModule.forRoot([], { initialNavigation: 'enabledBlocking' }),
