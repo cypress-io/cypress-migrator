4be8e793d87c5bf224c60bd9f161a36b4fef84d9
Jordan <jordan@jpdesigning.com>
2022-01-04 11:51:39 -0500

    test(migrator): fix broken test harness

--- apps/ng-migrator/src/app/home/editor/error-alerts/migration-errors/migration-errors.component.spec.ts
+++ apps/ng-migrator/src/app/home/editor/error-alerts/migration-errors/migration-errors.component.spec.ts
@@ -1,4 +1,5 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing'
+import { provideMockStore } from '@ngrx/store/testing'
 
 import { MigrationErrorsComponent } from './migration-errors.component'
 
@@ -9,6 +10,7 @@ describe('MigrationErrorsComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       declarations: [MigrationErrorsComponent],
+      providers: [provideMockStore()],
     }).compileComponents()
   })
 
