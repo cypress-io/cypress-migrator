c6747e6af9e62bb9bc50a3c3605c16bde72c1ebc
Paul Jaffre <jaffrepaul@gmail.com>
2022-05-18 10:44:04 -0400

    rebuild footer to match mocks

--- apps/migrator/components/footer.tsx
+++ apps/migrator/components/footer.tsx
@@ -1,66 +1,164 @@
 import { ReactElement } from 'react'
 
-/* This example requires Tailwind CSS v2.0+ */
-const navigation = [
-  {
-    name: 'Twitter',
-    href: 'https://twitter.com/Cypress_io',
-    icon: (props) => (
-      <svg fill="currentColor" viewBox="0 0 24 24" {...props}>
-        <path d="M8.29 20.251c7.547 0 11.675-6.253 11.675-11.675 0-.178 0-.355-.012-.53A8.348 8.348 0 0022 5.92a8.19 8.19 0 01-2.357.646 4.118 4.118 0 001.804-2.27 8.224 8.224 0 01-2.605.996 4.107 4.107 0 00-6.993 3.743 11.65 11.65 0 01-8.457-4.287 4.106 4.106 0 001.27 5.477A4.072 4.072 0 012.8 9.713v.052a4.105 4.105 0 003.292 4.022 4.095 4.095 0 01-1.853.07 4.108 4.108 0 003.834 2.85A8.233 8.233 0 012 18.407a11.616 11.616 0 006.29 1.84" />
-      </svg>
-    ),
-  },
-  {
-    name: 'GitHub',
-    href: 'https://github.com/cypress-io/cypress',
-    icon: (props) => (
-      <svg fill="currentColor" viewBox="0 0 24 24" {...props}>
-        <path
-          fillRule="evenodd"
-          d="M12 2C6.477 2 2 6.484 2 12.017c0 4.425 2.865 8.18 6.839 9.504.5.092.682-.217.682-.483 0-.237-.008-.868-.013-1.703-2.782.605-3.369-1.343-3.369-1.343-.454-1.158-1.11-1.466-1.11-1.466-.908-.62.069-.608.069-.608 1.003.07 1.531 1.032 1.531 1.032.892 1.53 2.341 1.088 2.91.832.092-.647.35-1.088.636-1.338-2.22-.253-4.555-1.113-4.555-4.951 0-1.093.39-1.988 1.029-2.688-.103-.253-.446-1.272.098-2.65 0 0 .84-.27 2.75 1.026A9.564 9.564 0 0112 6.844c.85.004 1.705.115 2.504.337 1.909-1.296 2.747-1.027 2.747-1.027.546 1.379.202 2.398.1 2.651.64.7 1.028 1.595 1.028 2.688 0 3.848-2.339 4.695-4.566 4.943.359.309.678.92.678 1.855 0 1.338-.012 2.419-.012 2.747 0 .268.18.58.688.482A10.019 10.019 0 0022 12.017C22 6.484 17.522 2 12 2z"
-          clipRule="evenodd"
-        />
-      </svg>
-    ),
-  },
-  {
-    name: 'YouTube',
-    href: 'https://www.youtube.com/channel/UC-EOsTo2l2x39e4JmSaWNRQ',
-    icon: (props) => (
-      <svg fill="currentColor" viewBox="10 8 28 28" {...props}>
-        {/* <rect width="48" height="48" rx="12"></rect> */}
-        <path
-          fillRule="evenodd"
-          d="M36.83 18.556c0-2.285-1.681-4.124-3.758-4.124a184.713 184.713 0 00-8.615-.182h-.914c-2.925 0-5.799.05-8.612.183-2.072 0-3.753 1.848-3.753 4.133A75.6 75.6 0 0011 23.99a78.487 78.487 0 00.173 5.429c0 2.285 1.68 4.139 3.753 4.139 2.955.137 5.987.198 9.07.192 3.087.01 6.11-.054 9.069-.193 2.077 0 3.758-1.853 3.758-4.138.121-1.813.177-3.62.172-5.434a73.982 73.982 0 00-.165-5.428zM21.512 28.97v-9.979l7.363 4.987-7.363 4.992z"
-          clipRule="evenodd"
-        />
-      </svg>
-    ),
-  },
-]
+const footerData = {
+  primary: [
+    {
+      heading: "Getting Started",
+      links: [
+        {
+          text: "Why Cypress",
+          href: "https://on.cypress.io/why-cypress",
+        },
+        {
+          text: "Installing Cypress",
+          href: "https://on.cypress.io/installing-cypress",
+        },
+        {
+          text: "Write your first test",
+          href: "https://on.cypress.io/writing-your-first-test",
+        },
+        {
+          text: "Testing your app",
+          href: "https://on.cypress.io/testing-your-app",
+        }
+      ]
+    },
+    {
+      heading: "Developers",
+      links: [
+        {
+          text: "Documentation",
+          href: "https://docs.cypress.io",
+        },
+        {
+          text: "Changelog",
+          href: "https://on.cypress.io/changelog",
+        },
+        {
+          text: "Status site",
+          href: "https://www.cypressstatus.com",
+        },
+        {
+          text: "Contribute",
+          href: "https://on.cypress.io/contributing",
+        },
+        {
+          text: "Learn Cypress",
+          href: "https://learn.cypress.io",
+        }
+      ]
+    },
+    {
+      heading: "Community",
+      links: [
+        {
+          text: "Blog",
+          href: "https://www.cypress.io/blog",
+        },
+        {
+          text: "Events",
+          href: "https://www.cypress.io/events",
+        },
+        {
+          text: "Testimonials",
+          href: "https://www.cypress.io/testimonials",
+        },
+        {
+          text: "Ambassadors",
+          href: "https://www.cypress.io/ambassadors",
+        },
+        {
+          text: "What's New",
+          href: "https://www.cypress.io/whats-new",
+        }
+      ]
+    },
+    {
+      heading: "Company",
+      links: [
+        {
+          text: "About",
+          href: "https://www.cypress.io/about",
+        },
+        {
+          text: "Careers",
+          href: "https://www.cypress.io/careers",
+        },
+        {
+          text: "Case Studies",
+          href: "https://www.cypress.io/case-studies",
+        },
+        {
+          text: "Support",
+          href: "https://www.cypress.io/support",
+        },
+        {
+          text: "Contact Us",
+          href: "/contact-us",
+        }
+      ]
+    }
+  ],
+  secondary: [
+    {
+      text: "Privacy policy",
+      href: "https://www.cypress.io/privacy-policy",
+    },
+    {
+      text: "Terms of Service",
+      href: "https://www.cypress.io/terms-of-use",
+    },
+    {
+      text: "Security & Compliance",
+      href: "https://www.cypress.io/security",
+    }
+  ]
+}
+
 
 const Footer = (): ReactElement => {
-  const year = new Date().getFullYear()
   return (
-    <footer className="bg-white">
-      <div className="max-w-7xl mx-auto py-12 px-4 sm:px-6 md:flex md:items-center md:justify-between lg:px-8">
-        <div className="flex justify-center space-x-6 md:order-2">
-          {navigation.map((item) => (
-            <a key={item.name} href={item.href} className="text-gray-600 hover:text-gray-700">
-              <span className="sr-only">{item.name}</span>
-              <item.icon className="h-6 w-6" aria-hidden="true" />
-            </a>
-          ))}
-        </div>
-        <div className="mt-8 md:mt-0 md:order-1">
-          <p className="text-center text-base text-gray-600">
-            &copy; {year} <a href="https://on.cypress.io">Cypress.io.</a> All rights reserved. |{' '}
-            <a href="https://github.com/cypress-io/cypress-migrator/issues">Submit An Issue</a>
-          </p>
-        </div>
-      </div>
-    </footer>
+    <>
+      <footer className="bg-indigo-500 px-16 pt-14 pb-4 md:p-0">
+        <section className="max-w-7xl h-full mx-auto py-6 border-b border-indigo-400">
+          <nav className="flex flex-row justify-between">
+            {footerData.primary.map(({ heading, links }) => (
+            <div className="" key={heading}>
+              <p className="text-gray-50 text-sm font-medium mb-3">{heading}</p>
+              <ul className="space-y-2">
+                {links.map(({href, text}) => (
+                <li className="text-indigo-200 hover:text-white text-sm font-light" key={text}>
+                  <a href={href} target={`_blank`}>{text}</a>
+                </li>
+                ))}
+              </ul>
+            </div>
+            ))}
+          <div className="max-w-sm">
+            <h1 className="text-gray-50 text-sm font-medium mb-3">Stay in the loop</h1>
+            <p className="text-indigo-200 text-sm font-light mb-6">Sign up to our newsletter for monthly updates on product releases, news, and articles direct to your inbox.</p>
+              <form className="flex">
+                <input type="email" placeholder="Enter your email" name="email" className="px-3 h-10 w-64 text-sm"></input>
+                <button type="button" className="text-indigo-900 bg-indigo-100 hover:text-indigo-500 rounded-r px-0.5 w-28">Subscribe</button>
+              </form>
+            </div>
+          </nav>
+        </section>
+        <section className="flex flex-row text-indigo-200 text-sm font-light pb-20 pt-4 max-w-7xl h-full mx-auto">
+          <nav>
+            <ul className="flex flex-row space-x-6">
+              <li className="hover:text-white">© Cypress.io</li>
+              {footerData.secondary.map(({href, text}) => (
+              <li className="hover:text-white" key={text}>
+                <a href={href} target={`_blank`}>{text}</a>
+              </li>
+              ))}
+            </ul>
+          </nav>
+        </section>
+      </footer>
+    </>
   )
 }
+
 export default Footer
