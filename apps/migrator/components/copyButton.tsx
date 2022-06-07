2cf81f2eea68ca4992aa04910b845854668706f3
Paul Jaffre <jaffrepaul@gmail.com>
2022-06-07 16:40:30 -0400

    match hover button states

--- apps/migrator/components/copyButton.tsx
+++ apps/migrator/components/copyButton.tsx
@@ -16,7 +16,7 @@ const CopyButton = (): ReactElement => {
       <button
         type="button"
         title="Copy Button"
-        className="inline-flex items-center px-4 py-2 shadow-sm text-sm font-medium rounded-md text-indigo-500 bg-indigo-50 hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+        className="w-full sm:w-auto sm:max-w-1/2 inline-flex justify-center items-center px-4 py-2 shadow-sm text-sm font-medium rounded-md text-indigo-500 bg-indigo-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 transition duration-300 ease-in-out transform hover:-translate-y-0 hover:scale-105"
         onClick={copy}
       >
         <ClipboardCopyIcon className="h-5 w-5 text-indigo-500" />
