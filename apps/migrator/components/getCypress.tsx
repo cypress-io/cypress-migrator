e144f5ba4fc5b763d1dba967c68dbaab4c244d76
Paul Jaffre <jaffrepaul@gmail.com>
2022-06-16 12:30:35 -0400

    normalize header font color

--- apps/migrator/components/getCypress.tsx
+++ apps/migrator/components/getCypress.tsx
@@ -12,7 +12,7 @@ const GetCypress = (): ReactElement => {
 
   return (
     <section className="flex flex-col items-center justify-center md:mt-20 mt-10">
-      <h2 className="lg:text-4xl lg:mb-0 md:text-3xl md:mb-0 sm:text-2xl mb-5 font-semibold">Test your code, not your patience.</h2>
+      <h2 className="text-gray-900 lg:text-4xl lg:mb-0 md:text-3xl md:mb-0 sm:text-2xl mb-5 font-semibold">Test your code, not your patience.</h2>
       {!isMobile ? (
       <div className="flex flex-row gap-4 pt-8 pb-12">
         <NPMCopy />
