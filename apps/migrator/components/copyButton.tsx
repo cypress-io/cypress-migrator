bb986f09fc2505d0a35edef2d23cfb5a63fa557d
Paul Jaffre <jaffrepaul@gmail.com>
2022-06-27 16:39:57 -0400

    update copy button styles

--- apps/migrator/components/copyButton.tsx
+++ apps/migrator/components/copyButton.tsx
@@ -18,7 +18,7 @@ const CopyButton = (): ReactElement => {
       <button
         type="button"
         title="Copy Button"
-        className="w-full md:w-auto inline-flex justify-center items-center px-4 py-2 shadow-sm text-sm font-medium rounded-md text-indigo-500 bg-indigo-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 transition duration-300 ease-in-out transform hover:-translate-y-0 hover:scale-105"
+        className="w-full md:w-auto inline-flex justify-center items-center px-4 py-2 shadow-sm text-base font-medium rounded-md text-indigo-500 bg-indigo-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 hover:bg-indigo-100 transition duration-300 ease-in-out transform hover:-translate-y-0 hover:scale-105"
         onClick={copy}
       >
         <ClipboardCopyIcon className="h-5 w-5 text-indigo-500" />
