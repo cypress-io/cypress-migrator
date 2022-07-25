a1ba1c3744d9e92e0e8686b4ff68b7a06393cf5c
Adam Murray <adamm@cypress.io>
2022-07-25 12:08:51 -0500

    feat: add ga to track usage. add folders for image storage. remove unused images.

--- apps/migrator/components/logo.tsx
+++ apps/migrator/components/logo.tsx
@@ -8,7 +8,7 @@ interface Props {
 
 const Logo = forwardRef<HTMLAnchorElement, Props>(({ href, onClick }, ref) => (
   <a className="flex-shrink-0 flex items-center" href={href} onClick={onClick} ref={ref}>
-    <Image src="/logo_full.svg" alt="Cypress Logo" width="100" height="50" />
+    <Image src="/images/svg/logo_full.svg" alt="Cypress Logo" width="100" height="50" />
     <span className="text-xl font-light mx-1 mb-0.5 text-gray-1000">Migrator</span>
   </a>
 ))
